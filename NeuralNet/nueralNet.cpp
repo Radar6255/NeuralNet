f890ddfa2734993347a2e343c31712b583d761c9
radar6255 <radar6255@yahoo.com>
2019-03-21 10:28:39 -0400

    Need to change input and goal

--- NeuralNet/nueralNet.cpp
+++ NeuralNet/nueralNet.cpp
@@ -7,13 +7,16 @@ using namespace std;
 
 void run();
 void backprop(double, double);
-void testCases();
+double testCases();
 double errorAmount(int, int, double);
 
 //First we have to create the arrays for input, hidden, and output nodes
 int input[2];
 double hidden[1];
 double output[1];
+
+double dErrorda;
+double dErrordb;
 
 //Need to also store weights
 //First is input nodes then to hidden
@@ -30,8 +33,25 @@ int main(){
 	inToHidden[1][0] = rand()/((double) RAND_MAX);
 	hiddenToOut[0] = rand()/((double) RAND_MAX);
 
-	testCases();
+	cout << "The total error is " << testCases() << endl << endl;
 
+
+	backprop(dErrorda, dErrordb);
+	cout << "After back-propagation the error total is " << testCases();;
+
+}
+
+void run(){
+	//Find weighted sum for the hidden node
+	double sum = 0;
+	for (int i = 0; i < 2; i++){
+		sum += input[i] * inToHidden[i][0];
+	}
+	hidden[0] = sum;
+
+	output[0] = hidden[0] * hiddenToOut[0];
+}
+void findErrorInCases(){
 	input[0] = 1;
 	input[1] = 1;
 
@@ -48,7 +68,7 @@ int main(){
 	double daOut = errorAmount(input[0], input[1],output[0]);
 
 	//Finding dOut/da
-	double dErrorda = (out - daOut) / da;
+	dErrorda = (out - daOut) / da;
 	cout << "dError/da " << dErrorda << endl;
 
 	//Finding f(b-db)
@@ -58,25 +78,11 @@ int main(){
 
 	double dbOut = errorAmount(input[0], input[1],output[0]);
 
-	double dErrordb = (out - dbOut) / db;
+	dErrordb = (out - dbOut) / db;
 	cout << "dError/db " << dErrordb << endl;
 
-	backprop(dErrorda, dErrordb);
-
-	testCases();
-
 }
 
-void run(){
-	//Find weighted sum for the hidden node
-	double sum = 0;
-	for (int i = 0; i < 2; i++){
-		sum += input[i] * inToHidden[i][0];
-	}
-	hidden[0] = sum;
-
-	output[0] = hidden[0] * hiddenToOut[0];
-}
 
 void backprop(double dErrorda, double dErrordb){
 	inToHidden[0][0] -= dErrorda;
@@ -84,36 +90,45 @@ void backprop(double dErrorda, double dErrordb){
 }
 
 double errorAmount(int in1, int in2, double guess){
-	if (in1 == in2){
-		return abs(1 - guess);
+	if (in1 == in2 && in1 == 1){
+//		return abs(1 - guess);
+		return 1 - guess;
 	}
 	return guess;
 }
 
 
-void testCases(){
+double testCases(){
+	double errorSum = 0;
+
 	cout << endl << "Printing all combinations\n";
 	input[0] = 1;
 	input[1] = 1;
 	cout << "Trying: " << input[0] << " " << input[1] << endl;
 	run();
 	cout << output[0] << endl << endl;
+	errorSum += errorAmount(1,1,output[0]);
 
 	input[0] = 1;
 	input[1] = 0;
 	cout << "Trying: " << input[0] << " " << input[1] << endl;
 	run();
 	cout << output[0] << endl << endl;
+	errorSum += errorAmount(1,0,output[0]);
 
 	input[0] = 0;
 	input[1] = 1;
 	cout << "Trying: " << input[0] << " " << input[1] << endl;
 	run();
 	cout << output[0] << endl << endl;
+	errorSum += errorAmount(0,1,output[0]);
 
 	input[0] = 0;
 	input[1] = 0;
 	cout << "Trying: " << input[0] << " " << input[1] << endl;
 	run();
 	cout << output[0] << endl << endl;
+	errorSum += errorAmount(0,0,output[0]);
+
+	return errorSum;
 }
