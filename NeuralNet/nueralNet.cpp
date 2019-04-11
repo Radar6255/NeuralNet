e3dd971d3fa477368a50d6831ee355dc1b9cccba
radar6255 <radar6255@yahoo.com>
2019-04-11 11:41:56 -0400

    Retooling the program to predict the next character typed

--- NeuralNet/nueralNet.cpp
+++ NeuralNet/nueralNet.cpp
@@ -5,21 +5,24 @@
 
 using namespace std;
 
+void runCase(char[]);
 void run();
 void backprop(double, double);
 double testCases();
 double errorAmount(int, int, double);
 double activationFunc(double);
+char readOut();
 
 //First we have to create the arrays for input, hidden, and output nodes
-const int numInputs = 2;
+const int numInputs = 5 * 27;
 int input[numInputs];
 
 const int numHidden = 1;
 double hidden[numHidden];
 
-const int numOut = 1;
+const int numOut = 27;
 double output[numOut];
+double outputError[numHidden][numOut];
 
 double dErrorda;
 double dErrordb;
@@ -28,7 +31,7 @@ double dErrordb;
 //First is hidden -> inputs so you can multiply each element in the inputs by hidden weights
 double inToHidden[numHidden][numInputs];
 
-//First is out -> inputs so you can multiply each element in the inputs by hidden weights
+//First is out -> hidden so you can multiply each element in the inputs by hidden weights
 double hiddenToOut[numOut][numHidden];
 
 int main(){
@@ -48,12 +51,11 @@ int main(){
 			hiddenToOut[out][hid] = rand()/((double) RAND_MAX);
 		}
 	}
+	char test[] = {'h','e','l','l','o',' '};
+	runCase(test);
+	cout << readOut();
 
-	cout << "The total error is " << testCases() << endl << endl;
-
-
-	backprop(dErrorda, dErrordb);
-	cout << "After back-propagation the error total is " << testCases();;
+//	cout << "The total error is " << testCases() << endl << endl;
 
 }
 
@@ -61,26 +63,31 @@ double activationFunc(double val){
 	return 1.0/(1.0+exp(-val));
 }
 
+double derivativeOfActivation(double val){
+	return -exp(val) * pow((1+exp(val)),-2);
+}
+
 void run(){
 	//Find weighted sum for the hidden nodes
 	for (int hid = 0; hid < numHidden; hid++){
 		hidden[hid] = 0;
 		for (int in = 0; in < numInputs; in++){
-			hidden[hid] += input[hid] * inToHidden[hid][in];
+			hidden[hid] += input[in] * inToHidden[hid][in];
 		}
 		//Applying activation function
 		hidden[hid] = activationFunc(hidden[hid]);
 	}
 
 	//Find weighted sum for the output nodes
-	for (int out = 0; out < numHidden; out++){
+	for (int out = 0; out < numOut; out++){
 		output[out] = 0;
-		for (int hid = 0; hid < numInputs; hid++){
-			output[out] += input[out] * inToHidden[out][hid];
+		for (int hid = 0; hid < numHidden; hid++){
+			output[out] += hidden[hid] * hiddenToOut[out][hid];
 		}
 		output[out] = activationFunc(output[out]);
 	}
 }
+
 void findErrorInCases(){
 	input[0] = 1;
 	input[1] = 1;
@@ -100,25 +107,29 @@ void findErrorInCases(){
 	//Finding dOut/da
 	dErrorda = (out - daOut) / da;
 	cout << "dError/da " << dErrorda << endl;
+	outputError[0][0] = dErrorda * derivativeOfActivation(output[0]);
 
 	//Finding f(b-db)
 	inToHidden[0][1] -= db;
 
 	run();
 
+
+
 	double dbOut = errorAmount(input[0], input[1],output[0]);
 
 	dErrordb = (out - dbOut) / db;
 	cout << "dError/db " << dErrordb << endl;
+	outputError[1][0] = dErrordb * derivativeOfActivation(output[0]);
 
 }
-
 
 void backprop(double dErrorda, double dErrordb){
 	inToHidden[0][0] -= dErrorda;
 	inToHidden[0][1] -= dErrordb;
 }
 
+//TODO Change when the goal changes
 double errorAmount(int in1, int in2, double guess){
 	if (in1 == in2 && in1 == 1){
 //		return abs(1 - guess);
@@ -127,8 +138,48 @@ double errorAmount(int in1, int in2, double guess){
 	return guess;
 }
 
+//Function to set the appropriate inputs for the the neural network
+void setInputs(char previous[numInputs/27]){
+	//Refreshing input to all 0's
+	for (int i = 0; i < numInputs; i++){
+		input[i] = 0;
+	}
+	//Adding a 1 for the character in the appropriate input
+	for (int i = 0; i < numInputs/27; i++){
+		char in = tolower(previous[i]);
+		if (in == ' '){
+			in = ((int) 'z')+1;
+		}
+		input[(i*27)+(in-97)] = 1;
+	}
+}
+
+//Function to read the output of the neural network
+char readOut(){
+	double greatest = output[0];
+	int greatestIndex = 0;
+	for (int i = 0; i < numOut; i++){
+		if (output[i] > greatest){
+			greatest = output[i];
+			greatestIndex = i;
+		}
+	}
+	return greatestIndex+97;
+}
+
+//Takes in the previous characters and the current character it should get
+void runCase(char chars[1+(numInputs/27)]){
+	char expected = chars[numInputs/27];
+	char previous[numInputs/27];
+	for (int i = 0; i < numInputs/27; i++){
+		previous[i] = chars[i];
+	}
+	setInputs(previous);
+	run();
+}
 
 double testCases(){
+	//TODO Change tests
 	double errorSum = 0;
 
 	cout << endl << "Printing all combinations\n";
